098c80a2dbd666a1c0783062aaafb3d4a18cf0d4
Joel Joby <joeljobyp@gmail.com>
2024-07-14 16:51:33 +0530

    did hello.html

---
+++ api/index.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask_cors import CORS, cross_origin
+from youtubesearchpython.__future__ import VideosSearch
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route("/app/test",methods=["GET"])
+def test_reply():
+    return {"messgage":"Suscess"}
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+@app.route('/about')
+def about():
+    return 'About'
+
+@app.route('/search/<query>')
+async def Search(query):
+    print("USER Queryied for:",query)
+    videosSearch = VideosSearch(query, limit = 20)
+    videosResult =  await videosSearch.next()
+    #print(videosResult["result"])
+    complete_data = []
+    print(len(videosResult["result"]))
+    for i in range(0,len(videosResult["result"])):
+        title = videosResult["result"][i]["accessibility"]["title"]
+        video_id = videosResult["result"][i]["id"]
+        preview = f"https://www.youtube.com/embed/{video_id}"
+        data = {"title":title,"preview":preview,"video_id":video_id}
+        complete_data.append(data)
+
+    
+    
+    #print(video_search_results["result"])
+    return {"data":complete_data,"total_results":len(videosResult["result"])}
+
+if __name__ == "__main__":
+    app.run(port=8000,debug=True)   
